b9ace41f9826ec2c3ea9c2ae29b207f272fcb3cc
sauravBarua <myallmailofficial@gmail.com>
2021-03-01 13:47:43 +0100

    Add exe-0

--- chapitre_01/index.js
+++ chapitre_01/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+app.get("/", (req, res) => {
+  res.send("Authors API");
+})
+
+
+
 
 app.listen(8000, () => {
     console.log("Server running........." );
