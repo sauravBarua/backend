d06d1ffac6f2425dc5080612b8f178a4c874a789
sauravBarua <myallmailofficial@gmail.com>
2021-03-01 13:59:58 +0100

    Add exe-1

--- chapitre_01/index.js
+++ chapitre_01/index.js
@@ -6,9 +6,27 @@ app.get("/", (req, res) => {
   res.send("Authors API");
 })
 
+app.get("/authors/:id", (req, res) => {
+  switch (req.params.id) {
+    case "1":
+      res.send("Lawrence Nowell, UK")
+      break;
+    case "2":
+      res.send("William Shakespeare, UK")
+      break;
+    case "3":
+      res.send("Charles Dickens, US")
+      break;
+    case "4":
+      res.send("Oscar Wilde, UK")
+      break;
+
+  }
+})
+
 
 
 
 app.listen(8000, () => {
-    console.log("Server running........." );
-  });
+  console.log("Server running.........");
+});
