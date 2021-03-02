9378cd32c08e52610f167cef6e18029c0a92b23c
sauravBarua <myallmailofficial@gmail.com>
2021-03-02 15:21:13 +0100

    Working on it

--- formulaires/index.js
+++ formulaires/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const expressHandlebars = require("express-handlebars");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("fommulaires");
-  });
+
+app.engine("handlebars", expressHandlebars());
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
 
 app.listen(8000, () => {
