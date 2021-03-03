5b2312108af1baf7bb337fcf09e02ae0be38da50
sauravBarua <myallmailofficial@gmail.com>
2021-03-03 13:42:34 +0100

    Working on it

---
+++ express_integration/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require("express-handlebars");
+const app = express();
+
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+
+app.listen(8000, () => {
+    console.log("Server running.........");
+});
