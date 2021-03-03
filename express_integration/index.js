8cc1c067d3c35dc95b75d80723561c23deb725c5
sauravBarua <myallmailofficial@gmail.com>
2021-03-03 17:16:50 +0100

    Update

--- express_integration/index.js
+++ express_integration/index.js
@@ -12,6 +12,26 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.get("/post", (req, res) => {
+    res.render("post");
+});
+
+app.get("/recipe", (req, res) => {
+    res.render("recipe");
+});
+
+app.get("/team-member", (req, res) => {
+    res.render("team-member");
+});
+
+app.get("/team", (req, res) => {
+    res.render("team");
+});
+
 
 
 app.listen(8000, () => {
