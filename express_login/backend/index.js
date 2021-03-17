a6834a337d5533246f04a79899812ca070eacac7
sauravBarua <myallmailofficial@gmail.com>
2021-03-17 13:39:56 +0100

    Working with express-validator

--- express_login/backend/index.js
+++ express_login/backend/index.js
@@ -3,17 +3,35 @@ const app = express();
 const port = 8000;
 const mongoose = require('mongoose');
 const userModel = require("./models/user");
+const expressValidator = require("express-validator");
+
 
 
 app.use(express.json());
 
+
 mongoose.connect('mongodb://localhost:27017/login', 
 {useNewUrlParser: true, useUnifiedTopology: true },
 ()=>{
-    console.log("db concoct........");
+    console.log("db connect........");
 });
 
 app.get('/',(req, res)=>{
-    res.send('Hello World')
+    res.json('Hello World')
 });
+
+app.post('/login', async (req, res) => {
+    const user = new userModel({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    try {
+        const saveUser = await user.save()
+        res.json(saveUser)
+
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
 app.listen(port);
--- express_login/backend/models/user.js
+++ express_login/backend/models/user.js
@@ -1,13 +1,11 @@
-const { Schema } = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  email: String,
+  password: String,
+});
 
-const userSchema = new Schema({
-    username: { type: String, unique: true },
-    email: String,
-    password: String
-})
-
-const userModel = mongoose.model('users', userSchema);
+const userModel = mongoose.model("users", userSchema);
 
 module.exports = userModel;
