4a3cc06322cd352d9e8bf4a6f87d6dfbab7bc4a2
sauravBarua <myallmailofficial@gmail.com>
2021-03-17 14:14:30 +0100

    express-validator success

--- express_login/backend/index.js
+++ express_login/backend/index.js
@@ -3,35 +3,42 @@ const app = express();
 const port = 8000;
 const mongoose = require('mongoose');
 const userModel = require("./models/user");
-const expressValidator = require("express-validator");
+const { body, validationResult } = require('express-validator');
 
 
 
 app.use(express.json());
 
 
-mongoose.connect('mongodb://localhost:27017/login', 
-{useNewUrlParser: true, useUnifiedTopology: true },
-()=>{
-    console.log("db connect........");
-});
+mongoose.connect('mongodb://localhost:27017/login',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("db connect........");
+    });
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.json('Hello World')
 });
 
-app.post('/login', async (req, res) => {
-    const user = new userModel({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    })
-    try {
-        const saveUser = await user.save()
-        res.json(saveUser)
+app.post('/login',
+    body('email').isEmail(),
+    body('password').isLength({ min: 8 }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const user = new userModel({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        try {
+            const saveUser = await user.save()
+            res.json(saveUser)
 
-    } catch (err) {
-        res.json({ message: err })
-    }
-})
+        } catch (err) {
+            res.json({ message: err })
+        }
+    })
 app.listen(port);
