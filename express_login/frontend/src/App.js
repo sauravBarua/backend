93987e2adbf45540963991c48027a90738dc0a75
sauravBarua <myallmailofficial@gmail.com>
2021-03-18 12:30:54 +0100

    Use useState

--- express_login/frontend/src/App.js
+++ express_login/frontend/src/App.js
@@ -1,20 +1,31 @@
 
+import React, { useState } from 'react';
 
 
-function Signup () {
+function Register () {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  function Signup (){
+    const items = {username,email,password}
+    console.log(items)
+
+  }
+
   return (
     <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
       <h1>Register page</h1>
-      <input type="text" className= "form-control" placeholder = "User name" />
+      <input type="text" value = {username} onChange = {(e)=>setUsername(e.target.value)} className= "form-control" placeholder = "User name" />
       <br/>
-      <input type="email" className = "form-control" placeholder = "Email"  />
+      <input type="email" value = {email} onChange = {(e)=>setEmail(e.target.value)} className = "form-control" placeholder = "Email"  />
       <br/>
-      <input type="password" className = "form-control" placeholder = "Password" />
+      <input type="password" value = {password} onChange = {(e)=>setPassword(e.target.value)} className = "form-control" placeholder = "Password" />
       <br/>
-      <button className = "btn btn-primary" >Sign up</button>
+      <button onClick = {Signup} className = "btn btn-primary" >Sign up</button>
       
     </div>
   );
 }
 
-export default Signup;
+export default Register;
