315e0005c05f8bdab6975cfcf552c367832cc2be
sauravBarua <myallmailofficial@gmail.com>
2021-03-23 16:50:09 +0100

    Add students

---
+++ database/sql/students.sql
@@ -0,0 +1,15 @@
+CREATE TABLE students (
+    ID int,
+    Name varchar(255),
+    City varchar(255)
+);
+insert into students(id, Name,City) 
+values
+(1, "Véronique","Paris"),
+('1', 'Véronique','Paris'),
+('2', 'Steeven','Lyon'),
+('3', 'Marc','Marseille'),
+('4', 'Nour','Lyon'),
+('5', 'Romain','Paris'),
+('6', 'Sophie','Paris');;
+select * from students;
