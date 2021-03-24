5508075d72fbd69856a82098da0d0395cda4b375
sauravBarua <myallmailofficial@gmail.com>
2021-03-24 10:53:37 +0100

    Add languages Table

--- database/sql/students.sql
+++ database/sql/students.sql
@@ -5,3 +5,6 @@ values(1, "Véronique","Paris"),
 ('4', 'Nour','Lyon'), 
 ('5', 'Romain','Paris'), 
 ('6', 'Sophie','Paris')
+
+
+INSERT INTO languages (ID,Name) VALUES(1,"French"),(2,"English"),(3,"German"),(4,"Spanish"),(5,"Mandarin")
