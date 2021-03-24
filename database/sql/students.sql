e795ada7ac9023c6059983609916d8cb4b7630bf
sauravBarua <myallmailofficial@gmail.com>
2021-03-24 11:06:30 +0100

    Add favorites Table

--- database/sql/students.sql
+++ database/sql/students.sql
@@ -6,5 +6,9 @@ values(1, "Véronique","Paris"),
 ('5', 'Romain','Paris'), 
 ('6', 'Sophie','Paris')
 
+CREATE TABLE languages (ID int,Name varchar(30))
 
 INSERT INTO languages (ID,Name) VALUES(1,"French"),(2,"English"),(3,"German"),(4,"Spanish"),(5,"Mandarin")
+
+CREATE TABLE favorites (id int, class varchar(30), sport varchar(30), student_id int )
+
